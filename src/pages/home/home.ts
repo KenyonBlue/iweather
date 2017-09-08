31406a2a4ef4b8365e19b3e49f67a2a76efeae5c
Kenyon Blue <bluekenyfleo@gmail.com>
2017-09-07 21:18:09 -0400

    added final changes and completed api weather app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -12,16 +13,22 @@ weather:any;
          city:string,
           state:string
      }
-  constructor(public navCtrl: NavController,private weatherProvider: WeatherProvider) {
+  constructor(public navCtrl: NavController,private weatherProvider: WeatherProvider, private storage: Storage) {
        
   }
      ionViewWillEnter(){
-          this.location = {
+          this.storage.get('location').then((val) => {
+             if(val != null){
+                  this.location = JSON.parse(val);
+             }  else {
+                  this.location = {
                city: 'lexington',
                state: 'Ky'
           }
-          this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
+             }
+               this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
                this.weather = weather.current_observation;
+               });
           });
      }
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
 /**
  * Generated class for the SettingsPage page.
  *
@@ -14,8 +14,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+     city: string;
+     state: string;
+     
+     
+  constructor(public navCtrl: NavController, 
+               public navParams: NavParams,
+              private storage: Storage) {
+       this.storage.get('location').then((val) => {
+          if(val != null){
+               let location = JSON.parse(val);
+               this.city = location.city;
+               this.state = location.state;
+          }     else  {
+               this.city = 'lexinigton';
+               this.state = 'Ky';
+          }  
+       });
   }
 
   ionViewDidLoad() {
